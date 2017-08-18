5f4f350090f8b68e5a4708515c5362b6f9a6f850
lee <lee@lee-pc>
2017-08-18 10:39:56 +0800

    修改controller

--- src/main/java/com/itzixi/controller/AlipayController.java
+++ src/main/java/com/itzixi/controller/AlipayController.java
@@ -4,16 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @Title: AlipayController.java
+ * @Package com.itzixi.controller
+ * @Description: controller
+ * Copyright: Copyright (c) 2016
+ * Company:FURUIBOKE.SCIENCE.AND.TECHNOLOGY
+ * 
+ * @author leechenxiang
+ * @date 2017年8月18日 上午10:39:15
+ * @version V1.0
+ */
 @Controller
 @RequestMapping("/alipay")
 public class AlipayController {
 	
-//	@Autowired
-//	private IUserService userService;
-
 	/**
 	 * 
-	 * @Description: TODO
+	 * @Description: 显示产品列表
 	 * @param model
 	 * @return
 	 * @throws Exception
@@ -23,9 +32,6 @@ public class AlipayController {
 	 */
 	@RequestMapping(value = "/products")
 	public String products(Model model) throws Exception {
-		
-//		User user = userService.findUserById(1);
-//		model.addAttribute("username", user.getName());
 		
 		return "products";
 	}
