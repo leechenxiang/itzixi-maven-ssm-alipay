c76c1cb00b6663fd275c9c70df71ed55765c5ff0
李晨翔 <leechenxiang@163.com>
2017-09-02 21:33:57 +0800

    基于web端的支付宝及时到账接口以及微信扫码支付

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -12,3 +12,39 @@ create table user
 );
 
 alter table user comment '用户表';
+
+
+CREATE TABLE `flow` (
+  `id` varchar(20) NOT NULL,
+  `flow_num` varchar(20) DEFAULT NULL COMMENT '流水号',
+  `order_num` varchar(20) DEFAULT NULL COMMENT '订单号',
+  `product_id` varchar(20) DEFAULT NULL COMMENT '产品主键ID',
+  `paid_amount` varchar(11) DEFAULT NULL COMMENT '支付金额',
+  `paid_method` int(11) DEFAULT NULL COMMENT '支付方式\r\n            1：支付宝\r\n            2：微信',
+  `buy_counts` int(11) DEFAULT NULL COMMENT '购买个数',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='流水表';
+
+
+CREATE TABLE `orders` (
+  `id` varchar(20) NOT NULL,
+  `order_num` varchar(20) DEFAULT NULL COMMENT '订单号',
+  `order_status` varchar(20) DEFAULT NULL COMMENT '订单状态\r\n            10：待付款\r\n            20：已付款',
+  `order_amount` varchar(11) DEFAULT NULL COMMENT '订单金额',
+  `paid_amount` varchar(11) DEFAULT NULL COMMENT '实际支付金额',
+  `product_id` varchar(20) DEFAULT NULL COMMENT '产品表外键ID',
+  `buy_counts` int(11) DEFAULT NULL COMMENT '产品购买的个数',
+  `create_time` datetime DEFAULT NULL COMMENT '订单创建时间',
+  `paid_time` datetime DEFAULT NULL COMMENT '支付时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单表';
+
+
+CREATE TABLE `product` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) DEFAULT NULL COMMENT '产品名称',
+  `price` varchar(11) DEFAULT NULL COMMENT '价格',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品表 ';
+
--- src/test/java/com/itzixi/test/service/UserServiceImplTest.java
+++ src/test/java/com/itzixi/test/service/UserServiceImplTest.java
@@ -14,67 +14,67 @@ import com.itzixi.service.UserService;
 
 public class UserServiceImplTest {
 
-	private ApplicationContext applicationContext;
-
-	@Before
-	public void setUp() throws Exception {
-
-		applicationContext = new ClassPathXmlApplicationContext(new String[] {
-				"spring/applicationContext-dao.xml",
-				"spring/applicationContext-transaction.xml",
-				"spring/applicationContext-service.xml"
-		});
-	}
-
-	@Test
-	public void testSaveUser() throws Exception {
-		
-		Sid sid = (Sid) applicationContext.getBean("sid");
-		
-		User user = new User();
-		user.setId(sid.nextShort());
-		user.setUsername("lcx");
-		user.setSex("男");
-		
-		UserService userService = (UserService) applicationContext.getBean("userService");
-		userService.saveUser(user);
-	}
-	
-	@Test
-	public void testGetUserById() throws Exception {
-		UserService userService = (UserService) applicationContext.getBean("userService");
-  
-		User user = userService.getUserById("1001");
-		System.out.println(user.toString());
-	}
-	
-	@Test
-	public void testGetUserList() throws Exception {
-		UserService userService = (UserService) applicationContext.getBean("userService");
-
-		List<User> userList = userService.getUserList();
-		for (User user : userList) {
-			System.out.println(user.toString());
-		}
-	}
-	
-	@Test
-	public void testUpdateUserById() throws Exception {
-		UserService userService = (UserService) applicationContext.getBean("userService");
-
-		User user = new User();
-		user.setId("1002");
-		user.setUsername("nathan");
-		user.setSex("woman");
-		
-		userService.updateUserById(user);
-	}
-	
-	@Test
-	public void testDeleteUserById() throws Exception {
-		UserService userService = (UserService) applicationContext.getBean("userService");
-
-		userService.deleteUserById("1002");
-	}
+//	private ApplicationContext applicationContext;
+//
+//	@Before
+//	public void setUp() throws Exception {
+//
+//		applicationContext = new ClassPathXmlApplicationContext(new String[] {
+//				"spring/applicationContext-dao.xml",
+//				"spring/applicationContext-transaction.xml",
+//				"spring/applicationContext-service.xml"
+//		});
+//	}
+//
+//	@Test
+//	public void testSaveUser() throws Exception {
+//		
+//		Sid sid = (Sid) applicationContext.getBean("sid");
+//		
+//		User user = new User();
+//		user.setId(sid.nextShort());
+//		user.setUsername("lcx");
+//		user.setSex("男");
+//		
+//		UserService userService = (UserService) applicationContext.getBean("userService");
+//		userService.saveUser(user);
+//	}
+//	
+//	@Test
+//	public void testGetUserById() throws Exception {
+//		UserService userService = (UserService) applicationContext.getBean("userService");
+//  
+//		User user = userService.getUserById("1001");
+//		System.out.println(user.toString());
+//	}
+//	
+//	@Test
+//	public void testGetUserList() throws Exception {
+//		UserService userService = (UserService) applicationContext.getBean("userService");
+//
+//		List<User> userList = userService.getUserList();
+//		for (User user : userList) {
+//			System.out.println(user.toString());
+//		}
+//	}
+//	
+//	@Test
+//	public void testUpdateUserById() throws Exception {
+//		UserService userService = (UserService) applicationContext.getBean("userService");
+//
+//		User user = new User();
+//		user.setId("1002");
+//		user.setUsername("nathan");
+//		user.setSex("woman");
+//		
+//		userService.updateUserById(user);
+//	}
+//	
+//	@Test
+//	public void testDeleteUserById() throws Exception {
+//		UserService userService = (UserService) applicationContext.getBean("userService");
+//
+//		userService.deleteUserById("1002");
+//	}
 	
 }
